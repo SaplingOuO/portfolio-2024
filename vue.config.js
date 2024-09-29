b22ea8fbff1bf001a69279e8e96d94ee67ffa51b
SBG_DESKTOP <gba2006su06@gmail.com>
2024-09-29 15:59:02 +0800

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
+
+module.exports = ({
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/MyPortfolio'
+  : '/',
+  // publicPath: './',
+  // outputDir:'./dist',
+  // assetsDir:'./static',
+  // filenameHashing: false,
+  // transpileDependencies: true,
 })
